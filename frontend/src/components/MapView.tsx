907a86e5b22cbdb9d68b0a665f1f3287dd7e6fd0
oscarperdana <sariminapp@gmail.com>
2026-03-14 11:41:52 +0700

    light mode MapView

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -102,7 +102,7 @@ export function MapView() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
           maxZoom={19}
         />
         <MapController center={mapCenter} zoom={mapZoom} />
